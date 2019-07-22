335a773829d334bf2933a8252b4c5a08deeb280f
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2019-07-22 09:54:17 +0200

    Improve misspelling and missing error check

--- any/any_example_test.go
+++ any/any_example_test.go
@@ -16,7 +16,7 @@ func ExamplePack() {
 	// call the Pack function to wrap your message. This returns an envelop type
 	_, err := any.Pack(&ac)
 	if err != nil {
-		fmt.Errorf("an error ocurred while packing the message: %s", err)
+		fmt.Printf("an error occurred while packing the message: %s", err)
 	}
 }
 
@@ -29,7 +29,7 @@ func ExamplePackAndMarshal() {
 	// call the PackAndMarshal function to wrap your message and already proto.Marshal it
 	_, err := any.PackAndMarshal(&ac)
 	if err != nil {
-		fmt.Errorf("an error ocurred while packing and marshalling the message: %s", err)
+		fmt.Printf("an error occurred while packing and marshalling the message: %s", err)
 	}
 }
 
@@ -42,16 +42,15 @@ func ExampleUnpack() {
 	// call the PackAndMarshal function to wrap your message and already proto.Marshal it
 	raw, err := any.PackAndMarshal(&ac)
 	if err != nil {
-		fmt.Errorf("an error ocurred while packing and marshalling the message: %s", err)
+		fmt.Printf("an error occurred while packing and marshalling the message: %s", err)
 	}
 
 	// declare your expected type
 	var expectedExample pb.Example
 
 	// call the Unpack function that will unwrap your message from the envelop
-	err = any.Unpack(raw, &expectedExample)
-	if err != nil {
-		fmt.Errorf("an error ocurred while unpacking the message: %s", err)
+	if err := any.Unpack(raw, &expectedExample); err != nil {
+		fmt.Printf("an error occurred while unpacking the message: %s", err)
 	}
 }
 
@@ -64,13 +63,13 @@ func ExampleGetMessageName() {
 	// call the PackAndMarshal function to wrap your message and already proto.Marshal it
 	raw, err := any.PackAndMarshal(&ac)
 	if err != nil {
-		fmt.Errorf("an error ocurred while packing and marshalling the message: %s", err)
+		fmt.Printf("an error occurred while packing and marshalling the message: %s", err)
 	}
 
 	// call the Unpack function that will unwrap your message from the envelop
 	name, err := any.GetMessageName(raw)
 	if err != nil {
-		fmt.Errorf("an error ocurred while getting the message name: %s", err)
+		fmt.Printf("an error occurred while getting the message name: %s", err)
 	}
 
 	fmt.Printf("message name: %s", name) // should print pb.Example
